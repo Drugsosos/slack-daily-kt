0476a05ee83e4fac61bff212e58171ede7675b42
Drugsosos <44712637+Drugsosos@users.noreply.github.com>
2023-01-10 15:22:07 +0400

    added Main.kt [WIP]; new dependencies to gradle; hibernate config; idea stuff to .gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,34 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.20"
+    val kotlinVersion by System.getProperties()
+    kotlin("jvm") version "$kotlinVersion"
+    id("org.jetbrains.kotlin.plugin.jpa") version "$kotlinVersion"
     application
 }
 
 group = "slack.daily.kt"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://repo.clojars.org")
+        name = "Clojars"
+    }
 }
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation("com.slack.api:bolt-socket-mode:1.26.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("com.slack.api:bolt-socket-mode:1.27.2")
     implementation("javax.websocket:javax.websocket-api:1.1")
-    implementation("org.slf4j:slf4j-simple:2.0.3")
-    implementation("org.glassfish.tyrus.bundles:tyrus-standalone-client:1.17")
+    implementation("org.slf4j:slf4j-simple:2.0.5")
+    implementation("org.glassfish.tyrus.bundles:tyrus-standalone-client:2.1.0")
+    implementation("org.hibernate:hibernate-core:6.1.6.Final")
+    implementation("org.glassfish.jaxb:jaxb-runtime:4.0.0")
+    implementation("org.postgresql:postgresql:42.5.1")
+    implementation ("io.github.microutils:kotlin-logging-jvm:3.0.4")
 }
 
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,42 +1,26 @@
-import com.slack.api.bolt.App
-import com.slack.api.bolt.AppConfig
 import com.slack.api.bolt.socket_mode.SocketModeApp
-import com.slack.api.model.Message.MessageItem
-import com.slack.api.model.block.Blocks.asBlocks
-import com.slack.api.model.block.Blocks.section
-import com.slack.api.model.block.composition.BlockCompositions.markdownText
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.time.Duration
+import mu.KotlinLogging
 
 
-fun main() {
-    val appConfig = AppConfig.builder()
-        .clientId("401593055731.4102576073862")
-        .clientSecret("41d2d1f36a8e17e479c319830183829a")
-//        .scope("app_mentions:read,chat:write,commands")
-        .oauthInstallPath("install")
-        .oauthRedirectUriPath("oauth_redirect")
-        .build()
+val logger = KotlinLogging.logger {}
 
-    // Initialize the App and register listeners
-    val app = App(appConfig)
+suspend fun launchBot() {
+    val applicationInstance = Listeners()
+    applicationInstance.addListeners()
 
-//    app.event(AppMentionEvent::class.java) { event, ctx ->
-//        ctx.say("<@${event.event.user}> What's up?")
-//        ctx.ack()
-//    }
+    val socketModeApp = SocketModeApp(applicationInstance.app)
+    socketModeApp.start()
 
-    app.command("/test") { req, ctx ->
-        // requires channels:join scope
-        ctx.client().conversationsJoin { it.channel(req.payload.channelId) }
-        ctx.say(asBlocks(
-            section { it.blockId("foo").text(markdownText("This is foo")) }
-        ))
-        ctx.ack()
+    delay(Duration.INFINITE)
+}
+
+
+fun main(): Unit = runBlocking {
+    launch {
+        launchBot()
     }
-
-
-
-    val appToken = "xapp-1-A0430GY25RC-4121829885025-725cb074d23baa20ecce12e45ea6a3337b1ffd2c834fc8c367f379044b2248e8"
-    val socketModeApp = SocketModeApp(appToken, app)
-
-    socketModeApp.startAsync()
 }
